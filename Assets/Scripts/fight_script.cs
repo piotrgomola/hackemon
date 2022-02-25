b1bf23abe30b0726ae8e84170c45d359f4bffbfe
maciej_b_moodup <maciej.b@moodup.team>
2022-02-25 23:31:19 +0100

    create fightScript

---
+++ Assets/Scripts/fight_script.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class fight_script : MonoBehaviour
+{
+    public GameObject me, oponent, tilemapWithButtons, tilemap;
+    public TextMeshPro infoText;
+    public Button catchButton, runButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onCatchButton_Click(){
+
+    }
+
+    public void onRunButton_Click(){
+        
+    }
+}
